42651a8e757147351b1aaa5c6a3e8d2f75880f79
ahas <dev@ahas.io>
2023-02-10 03:22:15 +0900

    update: Add test cases object, Vec2 type check

--- packages/test/test/type-check.spec.ts
+++ packages/test/test/type-check.spec.ts
@@ -7,19 +7,35 @@ describe("application loading", () => {
   });
 
   describe("types", () => {
-    it("will be converted from Buffer to Uint8Array", async () => {
-      const printBufferTypeButton = await browser.$("#print-buffer-type");
+    it("will be kept type from Date to Date", async () => {
+      const printButton = await browser.$("#print-object-type");
 
       await reset();
-      await printBufferTypeButton.click();
+      await printButton.click();
+      await expectMessage("true");
+    });
+
+    it("will be converted from Buffer to Uint8Array", async () => {
+      const printButton = await browser.$("#print-buffer-type");
+
+      await reset();
+      await printButton.click();
       await expectMessage("true");
     });
 
     it("will be kept type from Date to Date", async () => {
-      const printBufferTypeButton = await browser.$("#print-date-type");
+      const printButton = await browser.$("#print-date-type");
 
       await reset();
-      await printBufferTypeButton.click();
+      await printButton.click();
+      await expectMessage("true");
+    });
+
+    it("will be translated from Vec2 class to xy interface", async () => {
+      const printButton = await browser.$("#print-vec2-type");
+
+      await reset();
+      await printButton.click();
       await expectMessage("true");
     });
   });
